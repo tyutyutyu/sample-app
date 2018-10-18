2a606d642625b2ed00acc08564b3b355341753b0
István Földházi <istvan.foldhazi@gmail.com>
2018-10-18 23:51:37 +0200

    Upgrade tests to JUnit 5

---
+++ src/test/java/com/tyutyutyu/demo/sampleapp/SampleControllerTest.java
@@ -0,0 +1,47 @@
+package com.tyutyutyu.demo.sampleapp;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = SampleController.class)
+public class SampleControllerTest {
+
+	@Autowired
+	public MockMvc mockMvc;
+
+	@MockBean
+	public SampleService sampleService;
+
+	@Test
+	public void testStatus() throws Exception {
+
+		// given
+		String testStatus = "TEST";
+		when(sampleService.checkStatus()).thenReturn(testStatus);
+
+		// when
+		// @formatter:off
+		mockMvc.perform(get("/status").accept(MediaType.APPLICATION_JSON_UTF8))
+
+		// then
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(jsonPath("$.status", is(testStatus)));
+		// @formatter:on
+	}
+
+}
